cdcdf993650a3a1841ef583cfd2762e49d19023d
Chris Biertzer <c.biertzer@gmail.com>
2020-10-03 14:16:27 -0500

    save function

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,42 @@ $(document).ready(function() {
         localStorage.setItem(time, value);
     });
 
+    function updateTime() {
+        var currentTime = moment().hour();
+        $(".time-block").each(function() {
+            var timeBlock = parseInt($(this).attr("id").split("-")[1]);
+
+            if (timeBlock < currentTime) {
+                $(this).addClass("past");
+            }
+            else if (timeBlock === currentTime) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        });
+    }
+
+    updateTime();
+
+    var interval = setInterval(updateTime, 15000);
+
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  
+  
+
     $("#currentDay").text(moment().format("dddd, MMMM Do"));    
 });
 
